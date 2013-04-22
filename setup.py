a5ff00d2fe1b55d548161858052af1ebe376f47b
Priyank Pulumati <pulumati.priyank@gmail.com>
2013-04-21 20:57:50 -0700

    Python package for parsing log lines in the `logfmt` style. Changed meta information about package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bc-logfmt-python',
+  packages = ['bc-logfmt-python'],
+  version = '0.1',
+  description = 'Log formatter like GoLang\'s logfmt',
+  author = 'BC',
+  author_email = 'pulumati.priyank@gmail.com',
+  url = 'https://github.com/priyankcommits/logfmt-python',
+  download_url = 'https://github.com/priyankcommits/logfmt-python/archive/0.1.tar.gz',
+  keywords = ['format', 'logging', 'logfmt'],
+  classifiers = [],
+)
